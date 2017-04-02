928fdb3cc02a084b337954d7983ffbb09425f58b
jcalvoch <chepeman@gmail.com>
2017-04-01 18:30:48 -0600

    Clase FichaCliente

---
+++ FichaCliente.java
@@ -0,0 +1,45 @@
+public class FichaCliente 
+{
+		private Cliente dato;
+		private FichaCliente enlace;
+		private FichaMascota mascotas;
+		
+		public FichaCliente(Cliente c)
+		{
+			enlace = null;
+			dato = c;
+		}
+		
+		public FichaCliente(Cliente c, FichaCliente n)
+		{
+			enlace = n;
+			dato = c;
+		}
+		
+		public FichaCliente getEnlace()
+		{
+			return enlace;
+		}
+		
+		public Cliente getDato()
+		{
+			return dato;
+		}
+		
+		public void setEnlace(FichaCliente e)
+		{
+			enlace = e;
+		}
+		
+		public void setMascota(Mascota m)
+		{
+			mascotas = new FichaMascota(m, mascotas);
+		}
+		
+		public FichaMascota getMascota()
+		{
+			return mascotas;
+		}
+
+
+}
